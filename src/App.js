e09f2f90e3ad051b200093097404054a83c20099
zainulabidin776 <itsmezayynn@gmail.com>
2025-06-30 00:07:24 +0500

    first commit

---
+++ src/App.js
@@ -0,0 +1,21 @@
+import React from 'react'
+import './App.css';
+import {Route , Routes } from 'react-router-dom';
+import {Box} from '@mui/material';
+
+import ExerciseDetail from './pages/ExerciseDetail'
+import Home from './pages/Home'
+
+const App = () => {
+  return (
+    <Box width="400px">
+        <Navbar /> 
+        <Routes>
+            <Route path='/' element={<Home/>} />
+            <Route path="/exercise/:id" element={<ExerciseDetail />}/>
+        </Routes>
+    </Box>
+  )
+}
+
+export default App
